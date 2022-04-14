52eddd1dbc6e540c3fc26c6d72829676c21fdde0
ULAŞCAN UĞRAŞ <ugras.ulascan@hotmail.com>
2022-04-15 00:12:04 +0300

    Inheritance added.

---
+++ Inheritance/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public class Vehicle
+        {
+            public string Driver { get; set; }
+            public string Type { get; set; }
+        }
+
+        public class Plane :Vehicle //Inheritance
+        {
+            public string Model { get; set; }
+            public string Brand { get; set; }
+        }
+    }
+}
