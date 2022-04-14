4a9a35fade28e63c7a120b7341a5b8accce04237
ULAŞCAN UĞRAŞ <ugras.ulascan@hotmail.com>
2022-04-15 00:03:40 +0300

    Encapsulation added.

---
+++ Encapsulation/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            p1.Name= "Ali";
+            p1.Age = 33;
+
+
+            Console.WriteLine(p1.Tag());
+            Console.ReadKey();
+
+        }
+        public class Person
+        {
+            //Prop
+            public string Name { get; set; }
+
+            private int age; //field 
+
+
+            public int Age //property.
+            {
+                get
+                {
+                    return age;
+                }
+                set
+                {
+                    if (value < 0)
+                        throw new Exception("Age Cannot be Negative");
+                    age = value;
+                }
+            }
+
+
+
+            public string Tag()
+            {
+                return $"{Name} ({age} yasında)";
+            }
+
+        }
+    }
+}
